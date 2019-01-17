b19da5df4d741b910b968d91e4eeea9d56c21fe0
Inyouchange <38124145+Inyouchange@users.noreply.github.com>
2019-01-18 00:23:26 +0800

    Add files via upload

---
+++ 103ninja/103/GameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameViewController.swift
+//  103
+//
+//  Created by Betty on 03/10/2018.
+//  Copyright © 2018 Betty. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+        
+
+    }
+
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
